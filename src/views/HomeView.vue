102b2a453e72284aaa9432dd2469ac076d00696e
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-03-30 12:40:46 +0900

    add ckeditor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>CKEditor test</h1>
+    <CKEditor :editor="'ClassicEditor'" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import CKEditor from "@ckeditor/ckeditor5-vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    CKEditor,
   },
 });
 </script>
