d6d869db38095871016a7897c42ad001adc96838
masahirookubo <stmhamachiii@gmail.com>
2022-03-30 18:12:38 +0900

    added ckeditor vue lib

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { CKEditor } from "@ckeditor/ckeditor5-vue";
+import CKEditor from "@ckeditor/ckeditor5-vue";
+
 createApp(App).use(store).use(router).use(CKEditor).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
     <h1>CKEditor test</h1>
-    <CKEditor :editor="'ClassicEditor'" />
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import CKEditor from "@ckeditor/ckeditor5-vue";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    CKEditor,
+  components: {},
+  setup() {
+    return {
+      editor: ClassicEditor,
+      editorData: "<p>Content of the editor.</p>",
+      editorConfig: {},
+    };
   },
 });
 </script>
